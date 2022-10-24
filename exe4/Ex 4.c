d2927acd53a04d6a34bad7b2827093ef05c2ecca
RHAYSLON MATHEUS DE PAULA CUNHA <37088883+RhayslonDPaula@users.noreply.github.com>
2022-10-24 20:58:56 -0300

    PDFs 5,6 e 7.
    Adicionei os PDFs das listas 5,6,e7.

---
+++ exe4/Ex 4.c
@@ -0,0 +1,12 @@
+
+/*
+1-) Utilizando o método bolha, quantas trocas são necessárias para que a sequência [66, 84, 4,
+21, 71, 40] seja ordenada de modo crescente?
+*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  /* code */
+  return 0;
+}
