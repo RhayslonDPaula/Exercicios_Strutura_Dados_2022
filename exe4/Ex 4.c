293b44e6f7a8ebee6562b7fd7947fe21786ae6aa
RHAYSLON MATHEUS DE PAULA CUNHA <37088883+RhayslonDPaula@users.noreply.github.com>
2022-10-26 22:31:14 -0300

    Exe 4.2
    foi adicionado o segundo exercício da lista 4.

--- exe4/Ex 4.c
+++ exe4/Ex 4.c
@@ -1,6 +1,9 @@
 /*
 1-)  Utilizando o método bolha, quantas trocas são necessárias para que a sequência [66,  84,  4,  
 21,  71,  40] seja ordenada de modo crescente?
+
+2-) Utilizando o método bolha, quantas trocas são necessárias para que a sequência [0  52  86  34  
+82  7  5] seja ordenada de modo decrescente?
 */
 
 #include <stdio.h>
@@ -9,6 +12,7 @@
 # define N 6
 
 void ex1 (int b[]);
+void ex2 (int o);
 void troca (int *x,int *y);
 void imprime(int vet[]);
 int main(int argc, char const *argv[])
@@ -18,6 +22,9 @@ int main(int argc, char const *argv[])
   imprime(v);
   ex1(v);
   imprime(v);
+  int t[] = {0,52,86,34,82,7,5};
+ 
+  exe2(t);
   return 0;
 }
 void ex1(int b[]){
@@ -34,6 +41,23 @@ for ( i = 0; i < N; i++)
    
   }
   
+}
+void ex2 (int o){
+  int i,j,u=0;
+  for ( i = 0; i < N; i++)
+  {
+    for ( j = 1; j < N; j++)
+    {
+      if (o[j-1] < o[j])
+      {
+        troca(&o[j],&o[j-1]);
+        u++;
+      }
+      
+    }
+    
+  }
+  printf("numero de trocas%d",u);
 }
 }
 void troca (int *x,int *y){
